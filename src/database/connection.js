7a3f146ca9c5edb0e67489295f8ff2410e3fe62e
MichaelFranklyn <mfranklyn@outlook.com>
2022-10-17 12:47:23 -0300

    config

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,12 +1,11 @@
 const knex = require("knex")({
   client: "pg",
   connection: {
-    host: "ec2-52-23-131-232.compute-1.amazonaws.com",
-    user: "rhgqpwevyuiwou",
-    password:
-      "6798328a0fa2a4bc1ae6e6341c54c8b8443097cb8b26df0ddff81bb4b8f7a0ce",
-    database: "dcq9fp5883onj3",
-    port: 5432,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
     ssl: { rejectUnauthorized: false },
   },
 });
